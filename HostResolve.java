f0e4df0599564e4d5762b2d2b3107798a9c59716
nykrphguy123 <50631786+nykrphguy123@users.noreply.github.com>
2019-06-11 07:26:09 -0400

    ability to resolve hostnames
    feature is located in the JMenu

---
+++ HostResolve.java
@@ -0,0 +1,27 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class HostResolve {
+
+    private final String hostaddress;
+
+    public HostResolve(final String hostaddress) {
+
+        this.hostaddress = hostaddress;
+
+    }
+
+    public void testResolve(String name) {
+
+        try {
+
+            InetAddress address = InetAddress.getByName(hostaddress);
+
+            System.out.println("Resolved address is: " + address.getHostAddress());
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Unable to pull address");
+        }
+    }
+}
+
